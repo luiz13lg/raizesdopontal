370c2b3bf352e91f5c591fd9a7984b35cb810c43
Guilherme Couto Fernandes <gui_coutof@hotmail.com>
2018-09-25 11:45:59 -0300

    Login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,9 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
 
 	public function index(){
+		
 		$this->load->view('headerIndex');
 		$this->load->view('login');
 		$this->load->view('footer');
+	}
+
+	public function autentication(){
+
+		$email = $this->input->post("email");
+        $senha = $this->input->post("senha");
 	}
 
 }
--- application/views/login.php
+++ application/views/login.php
@@ -5,14 +5,14 @@
 	<h2> Login </h2>
 </div>
 <div class="container">
-	<form>
+	<form  method="post" action="<?= base_url()?>Login/autentication">
 	  <div class="form-group">
 	    <label for="exampleInputEmail1">Email</label>
-	    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+	    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
 	  </div>
 	  <div class="form-group">
 	    <label for="exampleInputPassword1">Senha</label>
-	    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	    <input type="password" class="form-control" id="senha" name="senha" placeholder="Password" required>
 	  </div>
 	  <button type="submit" class="btn btn-success">Entrar</button>
 	</form>
