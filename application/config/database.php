9680bb07cb66c7891e76eb8b4fa76b04b7732fa9
Luiz G <luiz13lg13@gmail.com>
2018-10-21 16:13:48 -0300

    Conexão com banco

--- application/config/database.php
+++ application/config/database.php
@@ -70,15 +70,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'raizesdopontal';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['raizesdopontal'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'raizesdopontal',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
